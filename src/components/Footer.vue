7339658931fa29e1b1ec9e708291eb81b8567720
Kangmin Seo <kangrnin@gmail.com>
2022-03-30 14:28:23 +0000

    Align footer text to center

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="absolute bottom-0 pb-6">
-    <p class="text-center text-xs text-gray-400">&copy; Kangmin Seo 2022</p>
+  <div class="absolute left-1/2 bottom-0 pb-6">
+    <p class="relative -left-1/2 text-center text-xs text-gray-400">
+      &copy; Kangmin Seo 2022
+    </p>
   </div>
 </template>
 
