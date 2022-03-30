788caeaf3e4594bf95168c31dc99a7f63f8d1682
Kangmin Seo <kangrnin@gmail.com>
2022-03-30 04:52:36 +0000

    Add router-links to header

---
+++ src/components/ColorRouterLink.vue
@@ -0,0 +1,18 @@
+<template>
+  <router-link
+    :to="to"
+    class="hover:text-blue-400 transition-all ease-in"
+    :class="$route.path == to ? 'text-blue-400' : 'text-gray-300'"
+  >
+    <slot></slot>
+  </router-link>
+</template>
+
+<script>
+export default {
+  name: 'ColorRouterLink',
+  props: {
+    to: String,
+  },
+}
+</script>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,23 @@
 <template>
-  <div class="mb-6">
-    <h2 class="text-2xl text-center">Habit Tracker</h2>
+  <div class="mb-8 text-center">
+    <h2 class="text-2xl mb-6">Habit Tracker</h2>
+    <div>
+      <ColorRouterLink to="/" class="mr-5">Home</ColorRouterLink>
+      <ColorRouterLink to="/about" class="mr-5">About</ColorRouterLink>
+      <ColorRouterLink to="/today" class="mr-5">Today</ColorRouterLink>
+      <ColorRouterLink to="/manage" class="mr-5">Manage</ColorRouterLink>
+    </div>
   </div>
 </template>
 
 <script>
+import ColorRouterLink from './ColorRouterLink.vue'
+
 export default {
   name: 'Header',
+  components: {
+    ColorRouterLink,
+  },
 }
 </script>
 
