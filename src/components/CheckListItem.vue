f288f2f28ba08f1c46587d9f0eaa76445a8729c5
Kangmin Seo <kangrnin@gmail.com>
2022-03-30 14:21:23 +0000

    Change CheckListItem font size, disable text select

--- src/components/CheckListItem.vue
+++ src/components/CheckListItem.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="text-xl cursor-pointer">
+  <div class="cursor-pointer">
     <i
-      class="fa fa-check-circle mr-1.5 transition-all ease-in"
+      class="text-xl fa fa-check-circle mr-1.5 transition-all ease-in"
       :class="checked ? 'text-blue-400' : 'text-gray-400'"
     ></i>
     <span
-      class="transition-all ease-in"
+      class="text-lg select-none transition-all ease-in"
       :class="checked && 'text-gray-300 line-through'"
       >{{ name }}</span
     >
